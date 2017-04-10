d46567e8744530ff34054a51a5bc2940243fd239
Anurag Datta Roy <anuragdattaroy@gmai.com>
2017-04-10 04:59:04 +0000

    Adding media root and restframework part 2

--- amsler/urls.py
+++ amsler/urls.py
@@ -39,3 +39,5 @@ urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += [
     url('^accounts/', include('django.contrib.auth.urls')),
 ]
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
